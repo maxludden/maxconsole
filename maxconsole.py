002cb152ecbcf9cb271e182a2d80be5fc7d887dd
maxludden <dev@maxludden.com>
2022-12-19 01:57:41 -0500

    Updating to 0.5.0

--- maxconsole.py
+++ maxconsole.py
@@ -1,40 +1,43 @@
 # maxconsole/maxconsole.py
 from rich.console import Console
+from rich.color import Color
 from rich.theme import Theme
 from rich.traceback import install as install_traceback
 import threading
 
 __version__ = '0.5.0'
 
-MAX_THEME = Theme(
-    {
-        "magenta": "#ff00ff",  #         #ff00ff
-        "purple": "#af00ff",  # P        #af00ff
-        "blue_violet": "#5f00ff",  #       #5f00ff
-        "blue": "bold #0000FF",  #       #0000ff
-        "cornflower_blue": "#249df1",  #   #249df1
-        "cyan": "#00ffff",  #            #00ffff
-        "green": "#00ff00",  #           #00ff00
-        "yellow": "#ffff00",  #          #ffff00
-        "orange": "#ff8800",  #          #ff8800
-        "red": "#ff0000",  #             #ff0000
-        "white": "#ffffff",  #           #ffffff
-        "light_grey": "#e2e2e2",  #        #e2e2e2
-        "grey": "#808080",  #            #808080
-        "dark_grey": "#2e2e2e",  #         #2e2e2e
-        "black": "#000000",  #           #000000
-        "debug": "bold bright_cyan",  #        #00ffff
-        "info": "bold cornflower_blue",  #     #249df1
-        "success": "bold bright_green",  #     #00ff00
-        "warning": "bold bright_yellow",  #    #ffff00
-        "error": "bold orange1",  #            #ff8800
-        "critical": "bold reverse red",#     #ff0000
-        "value": "bold bright_white",  #       #ffffff
-        "title": "bold purple",#             #af00ff
-        "key": "italic magenta",#            #ff00ff
-        "lines": "blue_violet",  #             #5f00ff
-    }
-)
+
+
+# MAX_THEME = Theme(
+#     {
+#         "magenta": "#ff00ff",  #         #ff00ff
+#         "purple": "#af00ff",  # P        #af00ff
+#         "blue_violet": "#5f00ff",  #       #5f00ff
+#         "blue": "bold #0000FF",  #       #0000ff
+#         "cornflower_blue": "#249df1",  #    #249df1
+#         "cyan": "#00ffff",  #            #00ffff
+#         "green": "#00ff00",  #           #00ff00
+#         "yellow": "#ffff00",  #          #ffff00
+#         "orange": "#ff8800",  #          #ff8800
+#         "red": "#ff0000",  #             #ff0000
+#         "white": "#ffffff",  #           #ffffff
+#         "light_grey": "#e2e2e2",  #        #e2e2e2
+#         "grey": "#808080",  #            #808080
+#         "dark_grey": "#2e2e2e",  #         #2e2e2e
+#         "black": "#000000",  #           #000000
+#         "debug": "bold bright_cyan",  #        #00ffff
+#         "info": "bold cornflower_blue",  #      #249df1
+#         "success": "bold bright_green",  #     #00ff00
+#         "warning": "bold bright_yellow",  #    #ffff00
+#         "error": "bold orange1",  #            #ff8800
+#         "critical": "bold reverse #aa0000",#     #ff0000
+#         "value": "bold bright_white",  #       #ffffff
+#         "title": "bold purple",#             #af00ff
+#         "key": "italic magenta",#            #ff00ff
+#         "lines": "blue_violet",  #             #5f00ff
+#     }
+# )
 class Singleton(type):
     _instance_lock = threading.Lock()
     def __init__(cls, *args, **kwargs):
@@ -51,11 +54,17 @@ class Singleton(type):
 
 class MaxConsole(Console, metaclass=Singleton):
     """A custom themed console class that inherits from rich.console.Console"""
-    theme: Theme
-    def __init__ (self, theme: Theme = MAX_THEME, *args, **kwargs):
+    theme: Theme = Theme.read('max_theme.ini')
+
+    def __init__ (self, theme: Theme = Theme.read('max_theme.ini'), *args, **kwargs):
         super().__init__(*args, **kwargs, theme=theme)
         install_traceback(console=self)
 
     def __call__(self, *args, **kwargs):
         return self
 
+
+if __name__ == "__main__":
+    console = MaxConsole()
+    console.print("Hello, world!")
+    assert isinstance(console, MaxConsole), f"Expected MaxConsole, got {type(console)}"
--- tests/maxconsole_tests.py
+++ tests/maxconsole_tests.py
@@ -1,14 +1,68 @@
-from maxconsole import MaxConsole
+import maxconsole as mc
+from rich.color import ANSI_COLOR_NAMES
 from rich.panel import Panel
+from rich.prompt import Prompt, Confirm
+from rich.table import Table, Column
+from rich import inspect
+
+
+color_sample = '◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎◼︎'
 
 def test_maxconsole():
-    console = MaxConsole()
-    assert isinstance(console, MaxConsole), f"Expected MaxConsole, got {type(console)}"
-    console.print(
-        Panel(
-            "Congratulations! You have successfully installed [bold magenta]MaxConsole[/bold magenta]!  "
-        )
+    console = mc.MaxConsole()
+
+    theme_color_test = Table(
+        title="MaxConsole Theme Test",
+        show_header=True,
+        show_lines=False,
+        show_edge=False,
+        width=80,
+        padding=(1,1)
     )
+    theme_color_test.add_column("Color", justify="left", no_wrap=True, ratio=1)
+    theme_color_test.add_column("Color Example", justify="center", no_wrap=True, ratio=3)
+    theme_color_test.add_row("magenta", f"[magenta]{color_sample}[/]")
+    theme_color_test.add_row("purple", f"[purple]{color_sample}[/]")
+    theme_color_test.add_row("blue_violet", f"[blue_violet]{color_sample}[/]")
+    theme_color_test.add_row("blue", f"[blue]{color_sample}[/]")
+    theme_color_test.add_row("cornflower_blue", f"[cornflower_blue]{color_sample}[/]")
+    theme_color_test.add_row("cyan", f"[cyan]{color_sample}[/]")
+    theme_color_test.add_row("green", f"[green]{color_sample}[/]")
+    theme_color_test.add_row("yellow", f"[yellow]{color_sample}[/]")
+    theme_color_test.add_row("orange", f"[orange]{color_sample}[/]")
+    theme_color_test.add_row("red", f"[red]{color_sample}[/]")
+    theme_color_test.add_row("white", f"[white]{color_sample}[/]")
+    theme_color_test.add_row("light_grey", f"[light_grey]{color_sample}[/]")
+    theme_color_test.add_row("grey", f"[grey]{color_sample}[/]")
+    theme_color_test.add_row("dark_grey", f"[dark_grey]{color_sample}[/DARK_GREY]")
+    theme_color_test.add_row("black", f"[black]{color_sample}[/]")
+    
+
+
+    try:
+        console.clear()
+        console.rule(title="MaxConsole Import Test", style="#00ffff",)
+        console.print()
+        console.print(
+            theme_color_test,
+            justify="center",
+        )
+        console.print()
+        console.print()
+        colors_correct = Confirm.ask("Are the above colors correct?", console=console, default='y', choices=['y', 'n'])
+        if colors_correct:
+            console.print(
+                Panel(
+                    "[bold #00ff00]Congratulations![/] You have successfully installed [italic #ff00ff]MaxConsole[/]!",
+                    title="[bold #ffffff]MaxConsole Import Test[/]",
+                    style="#00ffff",
+                    border_style="#ff00ff"
+                    
+                ),
+                justify="center",
+            )
+    except Exception as e:
+        inspect(e)
     
 if __name__ == "__main__":
     test_maxconsole()
